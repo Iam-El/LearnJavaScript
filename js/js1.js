b6ffb56c114e1490a6a6df734a83483527cbb448
Elsy Fernandes <elsyfernandes215@gmail.com>
2020-06-10 13:26:03 -0500

    first commit

---
+++ js/js1.js
@@ -0,0 +1,104 @@
+// basics on the language itself
+
+// content from w3schools
+
+// javascript  statements are composed of values , operators, expressions, comments
+
+
+// javascript - best practice to write the semicolons . although its not required
+
+// you can include spaces to make line more readable .
+
+// if javascript doesnt fit in one line then best place to break it at after the operator
+
+// some of the javascript keywords are
+
+// break, continue, debugger -> stops executing the javascript calls the debuger function
+
+//do .. while , for , function, if.. else , return , switch, try.. catch , var
+
+/*
+var x, y, z;
+x=1;
+x=5;
+
+*/
+
+// javascruipt values - fixed values and variables -> fixed values-literals
+
+
+// variables -> store data , var keyword declare variables, equal sign to assign a value to variables,
+
+var x;
+x=10;
+
+
+// operators->+,-,*,/
+
+// identifiers -> names to variables->starts with character , _, $ , case sensitive
+
+// names -<firstName,first_name,last_name
+// hyphesn are not allowed
+
+// it has unicode character set
+
+
+/// -------------------------------comments
+
+
+
+// single line comment
+/* multi line comment*/
+
+
+/// -----------------------------------variables
+
+//var and let keyword
+
+var elsy; // you just declare and dont assign a value its undefined
+
+// you can declare it tjis way
+
+var name1, name2, name3;
+
+var name1="elsy", name2="elsy2",name3="elsy4";
+
+// you can redeclare the variables in javascript
+
+var carname="volvo";
+var carname; // its still be volvo
+
+/// --------------------------------------concatenation
+
+var x="5"+2+3 // ans-> 523
+
+var y=2+3+"5"// ans 55
+
+
+/////-----------------------------------operators
+
+//+ ,-,*,**,/,%,++,--
+
+
+// assignment operator-> x=y -> x+=y ->x=x+y ->x-=y ->x=x-y -> x/=y->x=x/y ->x%=y->x=x%y
+
+
+
+///------------------------------------compariosion operator
+
+// == equal ,=== equal value and type , != -> not equal , !===-> not equal or not equal type, >->greater , < less than
+// >= greater eual to <= less than equal to ? ->ternary
+
+
+
+// ---------logical
+
+&& || !
+
+    // type operators
+// typeof -> type of variable
+// instanceof -> if the object is a instance of another object
+
+// operator precedence pemdas
+
+
